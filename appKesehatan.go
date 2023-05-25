c1198501dd570dbc3a43e27e21fe895cb2249cfa
dikaizm <dikamaah@gmail.com>
2023-05-25 17:01:47 +0700

    feat: add login feature

--- appKesehatan.go
+++ appKesehatan.go
@@ -24,6 +24,11 @@ type Pertanyaan struct {
 	konten string
 }
 
+type Tanggapan struct {
+	author User
+	konten string
+}
+
 type Forum struct {
 	tabPertanyaan [NMAX]Pertanyaan
 }
@@ -46,10 +51,10 @@ func guestMenu(users UserType) {
 		fmt.Scan(&opsi)
 
 		if opsi == 1 {
-			daftarUser(&users)
+			registerUser(&users)
 			opsiMenu()
 		} else if opsi == 2 {
-			// login if dokter/pasien
+			loginUser(users)
 		} else if opsi == 3 {
 			fmt.Println("Terima kasih! Sampai jumpa lagi :)")
 			return
@@ -66,81 +71,139 @@ func guestMenu(users UserType) {
 	}
 }
 
-func daftarUser(users *UserType) {
+func maxLen(users UserType) int {
+	if users.dokterLen > users.pasienLen {
+		return users.dokterLen
+	} else {
+		return users.pasienLen
+	}
+}
+
+func registerUser(users *UserType) {
 	var nama, username, password string
 	var isDokter string
-	var n int
+	var n int = maxLen(*users)
+	var hasUsername bool = false
 
-	fmt.Print("Masukkan nama: ")
-	fmt.Scan(&nama)
-	fmt.Print("Masukkan username: ")
-	fmt.Scan(&username)
-	fmt.Print("Masukkan password: ")
-	fmt.Scan(&password)
-
-	fmt.Print("Apakah Anda seorang dokter? (y/n): ")
-	fmt.Scan(&isDokter)
-
-	if strings.ToLower(isDokter) == "y" {
-		n = users.dokterLen
-
-		users.Dokter[n].nama = nama
-		users.Dokter[n].username = username
-		users.Dokter[n].password = password
-		users.dokterLen++
-	} else {
-		n = users.pasienLen
-
-		users.Pasien[n].nama = nama
-		users.Pasien[n].username = username
-		users.Pasien[n].password = password
-		users.pasienLen++
+	inputUser := func() {
+		fmt.Print("Masukkan nama: ")
+		fmt.Scan(&nama)
+		fmt.Print("Masukkan username: ")
+		fmt.Scan(&username)
+		fmt.Print("Masukkan password: ")
+		fmt.Scan(&password)
+		fmt.Print("Apakah Anda seorang dokter? (y/n): ")
+		fmt.Scan(&isDokter)
 	}
 
-	fmt.Println("\nPendaftaran berhasil!")
+	inputUser()
+
+	i := 0
+	for i < n {
+		if users.Dokter[i].username == username || users.Pasien[i].username == username {
+			fmt.Printf("\nUsername %s telah terdaftar. Ulangi proses pendaftaran!\n", username)
+			hasUsername = true
+			inputUser()
+		}
+	}
+
+	if !hasUsername {
+		if strings.ToLower(isDokter) == "y" {
+			n = users.dokterLen
+
+			users.Dokter[n].nama = nama
+			users.Dokter[n].username = username
+			users.Dokter[n].password = password
+			users.dokterLen++
+		} else {
+			n = users.pasienLen
+
+			users.Pasien[n].nama = nama
+			users.Pasien[n].username = username
+			users.Pasien[n].password = password
+			users.pasienLen++
+		}
+		fmt.Println("\nPendaftaran berhasil!")
+	}
 }
 
-func loginUser() {
+func loginUser(users UserType) {
+	var username, password string
+	var n int = maxLen(users)
+	var found int = 0
+	var isDokter bool
 
+	inputUser := func() {
+		fmt.Print("Masukkan username: ")
+		fmt.Scan(&username)
+		fmt.Print("Masukkan password: ")
+		fmt.Scan(&password)
+	}
+
+	inputUser()
+
+	i := 0
+	for i < n && found > 0 {
+		if users.Dokter[i].username == username && users.Dokter[i].password == password {
+			isDokter = true
+			found++
+		}
+		if users.Pasien[i].username == username && users.Pasien[i].password == password {
+			isDokter = false
+			found++
+		}
+		if found == 0 {
+			fmt.Println("Username atau password tidak valid")
+			inputUser()
+		}
+	}
+	if isDokter == true {
+		fmt.Println("login dokter")
+	} else {
+		fmt.Println("login pasien")
+	}
 }
 
-func lihatForum() {
+// func lihatForum() {
 
-}
+// }
 
-func cariTag() {
+// func cariTag() {
 
-}
+// }
 
-func lihatTagAtas() {
+// func lihatTagAtas() {
 
-}
+// }
 
-func postPertanyaan() {
+// func postPertanyaan() {
 
-}
+// }
 
-func postJawabn() {
+// func postJawaban() {
 
-}
+// }
 
-func pasienMenu() {
+// func pasienMenu() {
 
-}
+// }
 
-func dokterMenu() {
+// func dokterMenu() {
 
-}
+// }
 
 func debugUser(users UserType) {
 	fmt.Println("Dokter list")
 	for i := 0; i < users.dokterLen; i++ {
 		fmt.Printf("Nama: %s \tUsername: %s \tPass: %s\n", users.Dokter[i].nama, users.Dokter[i].username, users.Dokter[i].password)
 	}
+	fmt.Println(users.dokterLen)
+
 	fmt.Println("Pasien list")
 	for j := 0; j < users.pasienLen; j++ {
 		fmt.Printf("Nama: %s \tUsername: %s \tPass: %s\n", users.Pasien[j].nama, users.Pasien[j].username, users.Pasien[j].password)
 	}
+	fmt.Println(users.pasienLen)
 }
 
 func main() {
